21a9cc56cb0e7bd4a495c5c36b49bf9d5c8b54af
Matheus Mendonça Lima <matheus.mendonca@apoioprodesp.sp.gov.br>
2024-04-02 16:28:27 -0300

    feature/dev development new components Input, TextBlock and deletion of the  molecules.mdx file

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -5,12 +5,27 @@ export type InputProps = {
   React.InputHTMLAttributes<HTMLInputElement> |
   React.TextareaHTMLAttributes<HTMLTextAreaElement>
   )
-const Input = (props: InputProps) => {
-    if(props.multiline){
-      return <textarea {...props as React.TextareaHTMLAttributes<HTMLTextAreaElement>}/>
-    }
-    return <input {...props as React.InputHTMLAttributes<HTMLInputElement>}/>
+
+const InputOrTextArea = (props: InputProps) => {
+  if(props.multiline){
+    return <textarea {...props as React.TextareaHTMLAttributes<HTMLTextAreaElement>}/>
+  }
+  return <input {...props as React.InputHTMLAttributes<HTMLInputElement>}/>
+}
+const Input = ({label,className, ...rest}: InputProps) => {
+  return (
+    <div className="relative">
+      {label && <label className="absolute pt-1 pl-3 text-xs" >{label}</label>}
+      <InputOrTextArea 
+      className={`*
+      bg-dark text-gray-primary roubded-md p-3
+      disabled:bg-disabled disabled:border-2 disabled:border-disabled disabled:text-disabled
+      ${label && 'pt-5'}
+      ${className}
+      `}
+      {...rest}/>
+    </div>
+  )
 }
  
 export default Input
-
