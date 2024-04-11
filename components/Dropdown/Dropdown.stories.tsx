92321eb099cfc445cc6ee7aead363fbf78be6452
Matheus Mendonça Lima <matheus.mendonca@apoioprodesp.sp.gov.br>
2024-04-11 17:01:06 -0300

    feature/development Dropdown Component

---
+++ components/Dropdown/Dropdown.stories.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable storybook/story-exports */
+/* eslint-disable storybook/default-exports */
+import React from 'react'
+import {Meta, StoryObj} from '@storybook/react'
+import Dropdown, {type DropdownProps} from './Dropdown'
+
+const meta: Meta<typeof Dropdown> = {
+  title: 'Molecules/Dropdown',
+  component: Dropdown,
+  argTypes: {
+    list:{
+      description: "Use the side control to create the list that will be displayed in the dropdown",
+    }
+  },
+  decorators: [
+    (Story) => (
+      <div style={{paddingBottom: "15em", paddingTop: "3em"}}>
+        <Story/>
+      </div>
+    )
+  ]
+}
+
+export default meta;
+type Story = StoryObj<typeof Dropdown>
+
+export const Primary: Story = {
+  args: {
+    list: [
+    ],
+  },
+};
+
+export const Common: Story = {
+  args: {
+    list: [
+      "Durward Reynolds",
+      "Kenton Towne",
+      "Therese Wunsch",
+      "Benedict Kessler",
+      "Katelyn Rohan",
+    ],
+  },
+};
